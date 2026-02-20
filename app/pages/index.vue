a14edb46c0b2b75b4c825d91164c4aaadfdce46c
TatianaChF <tatiana.chefox@gmail.com>
2026-02-20 14:53:11 +0300

    add main page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,39 @@
 <template>
-  Test page
+  <div class="page">
+    <main class="main">
+      <h1>тестовое задание</h1>
+      <NuxtLink
+          to="#"
+          class="main__btn"
+      >
+        В каталог
+      </NuxtLink>
+    </main>
+  </div>
 </template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+.page {
+  min-height: calc(100vh - 30px - 30px);
+  display: flex;
+  align-items: center;
+}
+
+.main {
+  h1 {
+    font-weight: 500;
+    font-size: 64px;
+    margin-bottom: 40px;
+  }
+
+  &__btn {
+    padding: 10px 24px;
+    background-color: red;
+    color: white;
+    gap: 40px;
+  }
+}
+</style>
