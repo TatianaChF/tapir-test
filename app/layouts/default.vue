eafa6e41bf5d7819249d4c184a369fbb053b2e64
TatianaChF <tatiana.chefox@gmail.com>
2026-02-20 14:14:30 +0300

    add footer

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,9 +2,11 @@
   <div>
     <AppHeader />
     <slot />
+    <AppFooter />
   </div>
 </template>
 
 <script setup lang="ts">
 import AppHeader from "~/components/AppHeader.vue";
+import AppFooter from "~/components/AppFooter.vue";
 </script>
