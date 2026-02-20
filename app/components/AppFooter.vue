45edf2bf9157944332b2177e1531096a06b9ff68
TatianaChF <tatiana.chefox@gmail.com>
2026-02-20 14:14:20 +0300

    add footer

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,123 @@
+<template>
+  <footer class="footer">
+    <div class="footer__container">
+      <FooterNavBlock
+          titleCategory="Каталог"
+          :categoryLinks="catalog"
+      />
+
+      <FooterNavBlock
+          titleCategory="Помощь"
+          :categoryLinks="help"
+      />
+
+      <FooterNavBlock
+          titleCategory="Информация"
+          :categoryLinks="info"
+      />
+
+      <FooterNavBlock
+          titleCategory="Компания"
+          :categoryLinks="company"
+      />
+    </div>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+const catalog = [
+  {
+    text: "Все бренды",
+    link: "#",
+  },
+  {
+    text: "Nike",
+    link: "#",
+  },
+  {
+    text: "Adidas",
+    link: "#",
+  },
+  {
+    text: "Puma",
+    link: "#",
+  },
+  {
+    text: "New Balance",
+    link: "#",
+  },
+];
+
+const help = [
+  {
+    text: "Доставка",
+    link: "#",
+  },
+  {
+    text: "Заказ и оплата",
+    link: "#",
+  },
+  {
+    text: "Обмен и Возврат",
+    link: "#",
+  },
+  {
+    text: "F. A. Q.",
+    link: "#",
+  },
+];
+
+const info = [
+  {
+    text: "Преимущества",
+    link: "#",
+  },
+  {
+    text: "Гарантия и безопасность",
+    link: "#",
+  },
+  {
+    text: "Индивидуальный заказ",
+    link: "#",
+  },
+  {
+    text: "Акции",
+    link: "#",
+  },
+];
+
+const company = [
+  {
+    text: "О нас",
+    link: "#",
+  },
+  {
+    text: "Блог",
+    link: "#",
+  },
+  {
+    text: "Отзывы",
+    link: "#",
+  },
+  {
+    text: "Контакты",
+    link: "#",
+  },
+];
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: black;
+  color: white;
+  font-size: 14px;
+  line-height: 140%;
+  margin-left: -60px;
+
+  &__container {
+    padding: 100px 60px;
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
---
+++ app/components/FooterNavBlock.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="block">
+    <p>{{titleCategory}}</p>
+    <nav>
+      <ul class="block__nav">
+        <li v-for="item of categoryLinks">
+          <NuxtLink :to="item.link">{{item.text}}</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+export type categotyType = {
+  text: string;
+  link: string;
+}
+
+defineProps({
+  titleCategory: {
+    type: String,
+    required: true,
+  },
+  categoryLinks: {
+    type: Array<categotyType>,
+    required: true,
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.block {
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+
+  p {
+    color: #6F6F6F;
+  }
+
+  &__nav {
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+  }
+}
+</style>
